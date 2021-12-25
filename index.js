d13a350b18eefb85048f72711abe670ea813a1e7
Ashish Shukla <ashishfun008@gmail.com>
2021-12-26 01:50:50 +0530

    added get with sample todo data

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 import express from "express";
 const app = express();
-
+const todosData = [
+  {
+    todo: "Read the book",
+    _id: 1,
+  },
+  {
+    todo: "Call Someone",
+    _id: 2,
+  },
+  {
+    todo: "Complete the T",
+    _id: 3,
+  },
+];
 app.get("/", (req, res) => {
   res.send("root path response");
 });
 
 app.get("/api/todos", (req, res) => {
   res.json({
-    todos: [1, 2, 3],
+    todos: todosData,
   });
 });
 
