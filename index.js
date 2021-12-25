2ab4dccc6c7bcf90f69ce28f5e178e5111324e18
Ashish Shukla <ashishfun008@gmail.com>
2021-12-25 16:46:38 +0530

    added suport for es6 import and basic routing

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("starting ...");
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("root path response");
+});
+
+app.get("/api/todos", (req, res) => {
+  res.json({
+    todos: [1, 2, 3],
+  });
+});
+
+app.listen(3000, "localhost", () => {
+  console.log("listning ...");
+});
