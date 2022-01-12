2581cddf9d54ae7e6c6f83d17ffd4a24d53d7061
Ashish Shukla <ashishfun008@gmail.com>
2022-01-13 02:19:39 +0530

    added user register and login

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import lodash from "lodash";
+import bcrypt from "bcryptjs";
 
 dotenv.config();
 
@@ -19,7 +21,25 @@ const todoSchema = new mongoose.Schema({
     default: false,
   },
 });
+
+const userSchema = mongoose.Schema({
+  fullname: {
+    type: String,
+    required: false,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
 const Todo = mongoose.model("Todo", todoSchema);
+const User = mongoose.model("User", userSchema);
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -27,11 +47,66 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Home page of todo be");
-})
+});
 
 app.get("/api", (req, res) => {
   res.send("API page of todo be");
-})
+});
+
+app.post("/api/users", (req, MainRes) => {
+  const postedUserData = req.body;
+  const hash = bcrypt.hashSync(req.body.password);
+  User.exists({ email: req.body.email }, (req, userAlreadyExist) => {
+    if (userAlreadyExist) {
+      MainRes.status(409);
+      MainRes.json({
+        errorMsg: "User already exist",
+        _error: userAlreadyExist,
+      });
+    } else {
+      User.create({ ...postedUserData, password: hash }, (err, newUser) => {
+        if (err) {
+          MainRes.status(500);
+          MainRes.json({
+            errorMsg: "some error during Create",
+            _error: err,
+          });
+        }
+        MainRes.status(201);
+        MainRes.json(newUser);
+      });
+    }
+  });
+});
+
+app.get("/api/users/login", (req, res) => {
+  const userDetails = {
+    email: req.body.email,
+  };
+  User.findOne(userDetails, (err, savedUserData) => {
+    if (err || !savedUserData) {
+      res.json({
+        errorMsg: "Email or password is inccorrect",
+        _error: "Unauthorized",
+      });
+    } else {
+      if (bcrypt.compareSync(req.body.password, savedUserData.password)) {
+        const cud = {
+          ...savedUserData._doc,
+          password: null,
+          token: "dfsffasdasfaf",
+        };
+        res.json(cud);
+      } else {
+        res.status(401)
+        res.json({
+          errorMsg: "Email or password is inccorrect",
+          _error: "Unauthorized",
+        });
+      }
+    }
+  });
+});
 
 // To get the full list of todos
 app.get("/api/todos", (req, res) => {
@@ -73,7 +148,7 @@ app.delete("/api/todos/:id", (req, res) => {
 app.delete("/api/todos", (req, res) => {
   Todo.deleteMany({}, (req, deleteRes) => {
     res.json(deleteRes);
-  })
+  });
 });
 
 // to change the done status and edit the task
