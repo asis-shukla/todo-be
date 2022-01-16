6ea310196a524daa0b278cbcfda03a3386bd27e8
Ashish Shukla <ashishfun008@gmail.com>
2022-01-17 00:56:30 +0530

    added error handling and exist

--- src/api-routes/users.js
+++ src/api-routes/users.js
@@ -5,20 +5,39 @@ import User, {
   userLoginValidation,
 } from "../models/user-model.js";
 import jwt from "jsonwebtoken";
+import errorTypes from "./errorTypes.js";
 
 const router = express.Router();
+
+const errorResponse = (errorType, errorMessage, errorCode) => {
+  return {
+    error: {
+      type: errorType,
+      message: errorMessage,
+      code: errorCode,
+    },
+  };
+};
 
 router.post("/", async (req, MainRes) => {
   const postedUserData = req.body;
   const { error } = newUserValidation(postedUserData);
   if (error) {
-    return MainRes.status(400).json({ errorMsg: error.details[0].message });
+    return MainRes.status(400).json(
+      errorResponse(errorTypes.VALIDATION_ERROR, error.details[0].message, 400)
+    );
   }
   const hash = bcrypt.hashSync(req.body.password);
   try {
     const userAlreadyExist = await User.exists({ email: req.body.email });
     if (userAlreadyExist) {
-      return MainRes.status(409).json({ errorMsg: "User already exist" });
+      return MainRes.status(409).json(
+        errorResponse(
+          errorTypes.DUPLICATE_RESOURCE_ERROR,
+          "Email address already taken",
+          400
+        )
+      );
     } else {
       const newAddedUser = await User.create({
         ...postedUserData,
@@ -27,19 +46,41 @@ router.post("/", async (req, MainRes) => {
       return MainRes.status(201).json(newAddedUser);
     }
   } catch (error) {
-    MainRes.status(500).json(error);
+    MainRes.status(500).json(
+      errorResponse(
+        errorTypes.INTERNAL_SERVER_ERROR,
+        "Something goes wrong with server",
+        500
+      )
+    );
   }
 });
 
 router.get("/login", async (req, res) => {
   const { error } = userLoginValidation(req.body);
   if (error) {
-    return res.status(400).json({ errorMsg: error.details[0].message });
+    return res
+      .status(400)
+      .json(
+        errorResponse(
+          errorTypes.VALIDATION_ERROR,
+          error.details[0].message,
+          400
+        )
+      );
   }
   try {
     const savedUserData = await User.findOne({ email: req.body.email });
     if (!savedUserData) {
-      return res.status(404).json({ errorMsg: "User not found" });
+      return res
+        .status(404)
+        .json(
+          errorResponse(
+            errorTypes.RESOURCE_NOT_FOUND,
+            "This email is not registered",
+            400
+          )
+        );
     }
     const isValidPass = await bcrypt.compare(
       req.body.password,
@@ -49,16 +90,35 @@ router.get("/login", async (req, res) => {
       const userData = {
         fullname: savedUserData.fullname,
         email: savedUserData.email,
-        _id : savedUserData._id,
+        _id: savedUserData._id,
       };
       const tokenSecret = process.env.TOKEN_SECRET;
       const token = jwt.sign(userData, tokenSecret);
-      res.header("auth-token", token).status(200).json({ ...userData, token: token });
+      res
+        .header("auth-token", token)
+        .status(200)
+        .json({ ...userData, token: token });
     } else {
-      res.status(401).json({ errorMsg: "Email or password is incorrect" });
+      res
+        .status(401)
+        .json(
+          errorResponse(
+            errorTypes.INCORRECT_CREDENTIALS,
+            "Incorrect Password",
+            401
+          )
+        );
     }
   } catch (error) {
-    res.status(500).json(error);
+    res
+      .status(500)
+      .json(
+        errorResponse(
+          errorTypes.INTERNAL_SERVER_ERROR,
+          "Something goes wrong with server",
+          500
+        )
+      );
   }
 });
 
--- src/models/user-model.js
+++ src/models/user-model.js
@@ -19,16 +19,17 @@ const userSchema = mongoose.Schema({
 export const newUserValidation = (data) => {
   const userJoiSchema = Joi.object({
     fullname: Joi.string().min(3).max(50),
-    email: Joi.string().min(6).max(512).email(),
-    password: Joi.string().min(6).max(1024),
+    email: Joi.string().min(6).max(512).email().exist(),
+    password: Joi.string().min(6).max(1024).exist(),
   });
+
   return userJoiSchema.validate(data);
 };
 
 export const userLoginValidation = (data) => {
   const userJoiSchema = Joi.object({
-    email: Joi.string().min(6).max(512).email(),
-    password: Joi.string().min(6).max(1024),
+    email: Joi.string().min(6).max(512).email().exist(),
+    password: Joi.string().min(6).max(1024).exist(),
   });
   return userJoiSchema.validate(data);
 };
